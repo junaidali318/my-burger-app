35b3513be5f9481c10f1851aaeb8c767ea788eed
junaidali318 <junaidali318@gmail.com>
2020-06-30 19:43:43 +0500

    outputted orders through firebase

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,10 +3,31 @@ import React from 'react'
 import classes from './Order.css'
 
 const order = props => {
-    return(
+    const ingredients = []
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            amount: props.ingredients[ingredientName],
+            name: ingredientName
+        })
+    }
+
+    const ingredientOutput = ingredients.map(ig => (
+        <span
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                padding: '5px',
+                border: '1px solid black'
+            }}
+            key={ig.name}>{ig.name} ({ig.amount})</span>
+    ))
+
+    return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong>USD 5.45</strong></p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
         </div>
     )
 }
